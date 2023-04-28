d010a2b7dae81d4a0159ba17527388464e690090
Matheus Gois <124711079+dev-Gois@users.noreply.github.com>
2023-04-28 10:47:44 -0300

    Update README.js

--- README.js
+++ README.js
@@ -1,55 +1,68 @@
+```javascript
 class Matheus {
-  constructor() {
-    this.nome = "Matheus Alves Gois";
-    this.idade = 18;
-    this.nacionalidade = "Brasileiro";
-    this.idiomas = {
-      nativo: 'Português',
-      aprendendo: 'Inglês'
-    };
+    constructor() {
+      this.nome = "Matheus Alves Gois";
+      this.idade = 18;
+      this.nacionalidade = "Brasileiro";
+      this.idiomas = {
+        nativo: 'Português',
+        aprendendo: 'Inglês'
+      };
+    }
+  
+    contato() {
+      return {
+        discord: "goiiss#0001",
+        linkedin: "https://www.linkedin.com/in/matheus-gois-37659526b/",
+        email: "mattheusgoiis@gmail.com"
+      };
+    }
+  
+    formacao() {
+      return {
+        ensinoMedio: {
+          instituicao: "Adriano Nobre",
+          periodo: "2020-2022",
+          curso: "Técnico em Informática"
+        },
+        ensinoSuperior: {
+          instituicao: "UFC - Universidade Federal do Ceará",
+          semestre: "1º Semestre",
+          curso: "Análise e Desenvolvimento de Sistemas"
+        }
+      };
+    }
+  
+    coding() {
+      return {
+        linguagens: ['Python', 'JavaScript', 'Ruby', 'PHP', 'SQL'],
+        frameworks: ['Ruby on Rails', 'React'],
+        ide: ['Visual Studio Code', 'pyCharm'],
+        bancoDados: ['PostgresSQL', 'MySQL']
+      };
+    }
+  
+    experienciaProfissional() {
+      return {
+        primeiroEmprego: {
+          nomeEmpresa: "Digital Informática",
+          cargo: "Designer Gráfico",
+          periodo: "Agosto - 2022 à Março - 2023"
+        }
+      };
+    } 
   }
+  
+  const matheus = new Matheus();
+```
+<h2 align="center">Skills </h2>
 
-  contato() {
-    return {
-      discord: "goiiss#0001",
-      linkedin: "https://www.linkedin.com/in/matheus-gois-37659526b/",
-      email: "mattheusgoiis@gmail.com"
-    };
-  }
+<p align="center">
+  <a href="https://skillicons.dev">
+    <img src="https://skillicons.dev/icons?i=python,golang,vscode,androidstudio,c,cs,cpp,js,css,html" />
+  </a>
+</p>
 
-  formacao() {
-    return {
-      ensinoMedio: {
-        instituicao: "Adriano Nobre",
-        periodo: "2020-2022",
-        curso: "Técnico em Informática"
-      },
-      ensinoSuperior: {
-        instituicao: "UFC - Universidade Federal do Ceará",
-        semestre: "1º Semestre",
-        curso: "Análise e Desenvolvimento de Sistemas"
-      }
-    };
-  }
-
-  coding() {
-    return {
-      linguagens: ['Python', 'JavaScript', 'Ruby', 'PHP', 'SQL'],
-      frameworks: ['Ruby on Rails', 'React'],
-      ide: ['Visual Studio Code', 'pyCharm'],
-      bancoDados: ['PostgresSQL', 'MySQL']
-    };
-  }
-
-  experienciaProfissional() {
-    return {
-      primeiroEmprego: {
-        nomeEmpresa: "Digital Informática",
-        cargo: "Designer Gráfico",
-        periodo: "Agosto - 2022 à Março - 2023"
-      }
-    };
-  } 
-}
-
-const matheus = new Matheus();
+<p href="https://discord.gg/onlp" align="center">
+    <img alt="" src="https://github-readme-stats.vercel.app/api?username=xtekky&theme=tokyonight&show_icons=true">
+</p>
